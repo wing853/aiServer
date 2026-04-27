1aedf1000cdbe15f974abb07f7d9eb402be0f4c7
wing853 <qwqhsk@naver.com>
2026-04-27 16:31:41 +0900

    best_fix.pt파일 생성

--- test.py
+++ test.py
@@ -1,2 +1,11 @@
-import os
-print(os.path.exists("best.pt"))  # True 나와야 함
+import torch
+from ultralytics import YOLO
+
+# 1. 로컬에 있는 'best.pt'를 불러옵니다.
+model = YOLO('best.pt')
+
+# 2. 서버가 좋아하는 '투명 봉투(가중치 데이터)'로 새로 저장합니다.
+# 이 옵션이 서버 배포의 핵심입니다!
+torch.save(model.model.state_dict(), 'best_fix.pt', _use_new_zipfile_serialization=False)
+
+print("✅ 'best_fix.pt' 생성 성공! 이제 이 파일을 깃허브에 올리세요.")
